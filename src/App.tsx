4c1e5747e8519e62f700b7b535d2a60b8cb57b86
tkhrtkrthrth+rth+5t+- <fakharanabas@gmail.com>
2021-08-25 17:57:10 +0430

    add project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import profile from './img/team-4.jpg';
+function App() {
+  return (
+    <div className="App">
+     <h1>abas</h1>
+     <img src={profile} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
